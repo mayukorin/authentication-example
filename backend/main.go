83494a7a604262948300d37c86df073f0e4ea3bd
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2024-02-03 15:39:57 +0900

    #7 specify AllowHeaders, AllowMethods, and AllowOrigins to resolve CORS eror when using basic auth

--- backend/main.go
+++ backend/main.go
@@ -11,7 +11,23 @@ import (
 func main() {
 	router := gin.Default()
 	// allows all origins
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowMethods: []string{
+			"GET",
+			"OPTIONS",
+		},
+	}))
 
 	router.Use(middleware.BasicAuthMiddleware())
 	router.GET("/hello_with_basic_auth", handlers.GetHello)
